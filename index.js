6a8af866dc94df9af4d24101df375a0c4865f09e
Austin <aturn9880@gmail.com>
2021-06-04 18:20:55 -0400

    commit tree 9f6a9dae61558cade21c00fbb5c66d869fd6926a

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const body = document.querySelector('body');
+function submitData(name, email){
+    let userData = {
+        name: `${name}`,
+        email: `${email}`
+    };
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(resp => {return resp.json()})
+        .then(function(obj){
+            const para = document.createElement('p');
+            para.innerHTML = obj.id;
+            body.append(para);
+        })
+        .catch(function(error){
+            alert("Silly, we've got an ERROR!");
+            const para2 = document.createElement('p');
+            para2.innerHTML = error.message;
+            body.append(para2);
+        });
+}
